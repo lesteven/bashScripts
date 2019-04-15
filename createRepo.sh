8061ce8de857e6b681762f49edefea4d6bf87fc4
lesteven <le_steven@outlook.com>
2019-04-14 20:46:37 -0700

    refactor: update createRepo script

--- createRepo.sh
+++ createRepo.sh
@@ -3,9 +3,16 @@ read -p "Enter username: " username
 
 read -p "Enter reponame: " repo
 
-read -p "Private? true or false: " private
+read -p "Private? true(1) or false(0): " private
 
-curl -u $username https://api.github.com/user/repos -d \
-    '{"name": "'$repo'", "private": "'$private'"}' 
+if [ $private = true ] || [ $private = 1 ];then
+    curl -u $username https://api.github.com/user/repos -d \
+        '{"name": "'$repo'", "private": "'$private'"}' 
+else
+    curl -u $username https://api.github.com/user/repos -d \
+        '{"name": "'$repo'"}' 
+fi
+
+
 
 

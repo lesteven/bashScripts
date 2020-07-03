1a428aec43382739cc28c6c07f503917fcea8b2d
lesteven <stevenle2011@gmail.com>
2020-07-03 15:01:10 -0700

    aws: update ami script

--- aws/getUbuntuAmis.sh
+++ aws/getUbuntuAmis.sh
@@ -3,7 +3,7 @@
 # queries for ubuntu amis w/ ubuntu cloud account -> 18.04 amd64
 aws ec2 describe-images \
     --owners 099720109477 \
-    --filters 'Name=name,Values=*ubuntu/images/hvm-ssd/ubuntu-bionic-18.04*amd64*' \
+    --filters 'Name=name,Values=*ubuntu/images/hvm-ssd/ubuntu-*-20.04*amd64*' \
     --query 'reverse(sort_by(Images, &CreationDate))' \
     > ami.txt
 

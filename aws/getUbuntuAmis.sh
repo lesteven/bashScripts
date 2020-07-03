498374d8f4b0faa0ef61fc648417a9755a010fc3
lesteven <stevenle2011@gmail.com>
2020-07-03 14:51:25 -0700

    add aws ec2 script

---
+++ aws/getUbuntuAmis.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# queries for ubuntu amis w/ ubuntu cloud account -> 18.04 amd64
+aws ec2 describe-images \
+    --owners 099720109477 \
+    --filters 'Name=name,Values=*ubuntu/images/hvm-ssd/ubuntu-bionic-18.04*amd64*' \
+    --query 'reverse(sort_by(Images, &CreationDate))' \
+    > ami.txt
+
